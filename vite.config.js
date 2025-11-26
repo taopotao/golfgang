8f1bfb3a39f49daf99659feecce8801a6c9719b3
taopotao <tao_dinh@live.com>
2025-11-26 18:42:20 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // If your repo name is different, change '/golfgang/' to '/your-repo-name/'
   base: '/golfgang/'
 })
