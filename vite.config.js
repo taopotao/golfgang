3f07b8ee655daab53153632e69f43f87ae86f53e
taopotao <tao_dinh@live.com>
2025-11-29 12:16:30 +0000

    Force cache bust

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/golfgang/",
+  base: "/golfgang/?v=2",
   build: {
     cssCodeSplit: false,
     assetsInlineLimit: 0,
